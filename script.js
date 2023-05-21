80e5f4ce12567ea44fa977e4e96c89603bc6d693
duygubdy <duygubdy@gmail.com>
2023-05-21 20:28:12 +0300

    code fix

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
     var emailInput = $("#email");
     var phoneInput = $("#phone");
     var messageInput = $("#message");
-    var submit = $("#submit");
+    var submit = $("#submit-btn");
   
     topics.forEach(function(topic) {
       var option = $("<option></option>")
@@ -43,35 +43,35 @@ $(document).ready(function() {
     $("#contact-method input").checkboxradio({
       required: true
     });
-  
-    submit.on('click', function(e) {
-      e.preventDefault();
-  
-      var requiredFields = [problemDate, nameInput, emailInput, phoneInput, messageInput];
-      var filled = true;
-  
-      for (var i = 0; i < requiredFields.length; i++) {
-        if (requiredFields[i].val() === '') {
-          filled = false;
-          break;
-        }
-      }
-  
-      var radioChecked = $("#contact-method input:checked").length > 0;
-      if (!radioChecked) {
-        filled = false;
-      }
-  
-      if (filled) {
-        toastr.success('Form submitted successfully', '', { "progressBar": true, "positionClass": "toast-top-center", "timeOut": 3000, "closeButton": true, "showDuration": "300", "hideDuration": "1000"});
-        problemDate.val('');
-        nameInput.val('');
-        surnameInput.val('');
-        emailInput.val('');
-        phoneInput.val('');
-        messageInput.val('');
-      } else {
-        toastr.error('Starred fields cannot be left blank!', '', { "progressBar": true, "positionClass": "toast-top-center", "timeOut": 3000, "closeButton": true, "showDuration": "300", "hideDuration": "1000"});
-      }
-    });
+
   });
+    
+  function giveAlert() {
+
+    var requiredFields = [$('#date'), $('#name'), $('#email'), $('#phone'), $('#message')];
+    var filled = true;
+
+    for (var i = 0; i < requiredFields.length; i++) {
+      if (requiredFields[i].val() === '') {
+        filled = false;
+        break;
+      }
+    }
+
+    var radioChecked = $("#contact-method input:checked").length > 0;
+    if (!radioChecked) {
+      filled = false;
+    }
+
+    if (filled) {
+      toastr.success('Form submitted successfully', '', { "progressBar": true, "positionClass": "toast-top-center", "timeOut": 3000, "closeButton": true, "showDuration": "300", "hideDuration": "1000"});
+      $('#date').val('');
+      $('#name').val('');
+      $('#surname').val('');
+      $('#email').val('');
+      $('#phone').val('');
+      $('#message').val('');
+    } else {
+      toastr.error('Starred fields cannot be left blank!', '', { "progressBar": true, "positionClass": "toast-top-center", "timeOut": 3000, "closeButton": true, "showDuration": "300", "hideDuration": "1000"});
+    }
+  }
